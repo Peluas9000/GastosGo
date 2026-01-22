f21d378752c14ea44034c668741365b60e50f489
alumno <ayoubbenjaddi5@gmail.com>
2026-01-22 13:45:04 +0100

    Todas la interfaces hechas, falta las funcionalidades entre navegacion de activitys,room con sqlite y el adapter para la lista de gastos

--- app/src/main/java/com/ayoub/gastosgo/Login.kt
+++ app/src/main/java/com/ayoub/gastosgo/Login.kt
@@ -1,21 +1,30 @@
 package com.ayoub.gastosgo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val botonLogin = findViewById<Button>(R.id.btnLogin)
+        botonLogin.setOnClickListener {
+            val intento=Intent(this, MainActivity::class.java)
+                startActivity(intento)
+
+        }
+
     }
 }
